84b2ce16116d2ec76886a60abf919bd1e0379377
Duncan <duncanjosephwatts@gmail.com>
2023-02-06 15:05:26 +0100

    Updated labels

--- WMAP/01_reanalysis/figures/abscal_ame.py
+++ WMAP/01_reanalysis/figures/abscal_ame.py
@@ -4,13 +4,13 @@ import numpy as np
 
 DIR = '/mn/stornext/d5/data/duncanwa/WMAP'
 cg.plot(f'{DIR}/chains_grid_g0_1.178/ame_c0001_k000005.fits', min=-1e2, max=1e2, 
-    cmap='coolwarm', llabel=r'1.178', cbar=False, width=4)
+    cmap='coolwarm', llabel=r'g_0=1.178', cbar=False, width=4)
 plt.savefig('ame_g01.178.png', bbox_inches='tight', dpi=300)
 cg.plot(f'{DIR}/chains_grid_g0_1.181/ame_c0001_k000005.fits', min=-1e2, max=1e2, 
-    cmap='coolwarm', llabel='1.181', cbar=False, width=4)
+    cmap='coolwarm', llabel='g_0=1.181', cbar=False, width=4)
 plt.savefig('ame_g01.181.png', bbox_inches='tight', dpi=300)
 cg.plot(f'{DIR}/chains_grid_g0_1.185/ame_c0001_k000005.fits', min=-1e2, max=1e2, 
-    cmap='coolwarm', llabel='1.185', cbar=False, width=4)
+    cmap='coolwarm', llabel='g_0=1.185', cbar=False, width=4)
 plt.savefig('ame_g01.185.png', bbox_inches='tight', dpi=300)
 
 cg.standalone_colorbar("coolwarm", ticks=[-100,0,100], extend='both',
