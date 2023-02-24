63c4228f9a4338af5f2ff00db3346bc2b93b8e39
Duncan <duncanjosephwatts@gmail.com>
2023-02-24 19:16:53 +0100

    Updated figures

--- WMAP/01_reanalysis/figures/plot_psd.py
+++ WMAP/01_reanalysis/figures/plot_psd.py
@@ -93,6 +93,7 @@ with h5py.File(filename, 'r') as f:
         psd_r = np.abs(fft.rfft(r[detector] - n_corr[detector])) ** 2 / (n_samples * samprate)
         cut = 3
         slope = 0.7
+        slope = 0.5
         maxbin = 4000
         psd3 = bin_data_maxbin(psd, slope=slope, cut=cut, maxbin=maxbin)
         psd3_n = bin_data_maxbin(psd_n, slope=slope, cut=cut, maxbin=maxbin)
@@ -104,18 +105,20 @@ with h5py.File(filename, 'r') as f:
         S = wn * (f3 / fknee) ** (alpha)
         full = wn + S
         plt.figure(figsize=(5, 4))
-        plt.loglog(f3*1e3, 1e-3*psd3, 'C0', alpha=0.5, label=r'$d - g s_\mathrm{tot}$')
         plt.loglog(f3*1e3, 1e-3*psd3_r, 'k', alpha=0.5, label=r'$d - g s_\mathrm{tot} - n_\mathrm{corr}$')
         plt.loglog(f3*1e3, 1e-3*psd3_n, 'C1', alpha=0.5, label=r'$n_\mathrm{corr}$')
+        plt.loglog(f3*1e3, 1e-3*psd3, 'C0', alpha=0.5, label=r'$d - g s_\mathrm{tot}$')
 
-        plt.loglog(f3*1e3, 1e-3*full, '--', color='C0', label='current model (sample)')
         plt.loglog(f3*1e3, 1e-3*S, '--', color='C1') 
         plt.loglog(f3*1e3, 1e-3*(full * 0 + wn), '--', color='k')
+        plt.loglog(f3*1e3, 1e-3*full, '--', color='C0', label='current model (sample)')
         plt.xlim(4e-3, 2e4)
         plt.legend(frameon=False)
         
         plt.ylabel('PSD [du${}^2$ mHz${}^{-1}$]')
         plt.xlabel('Frequency [mHz]')
+        plt.ylim([1e-4, 5])
+        plt.xlim(xmax=1e4)
         #plt.savefig('ps_test_W4_det%i.png' % (detector+1), bbox_inches='tight', dpi=150)
         plt.savefig('ps_test_W4_det%i.pdf' % (detector+1), bbox_inches='tight')
         # plt.show()
