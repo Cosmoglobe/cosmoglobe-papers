76c9a061151ad573cd5537409109d963474ef34c
Duncan <duncanjosephwatts@gmail.com>
2023-03-10 16:38:58 +0100

    Updated some figures

--- WMAP/01_reanalysis/data/inst_plots/make_inst_plot_split.py
+++ WMAP/01_reanalysis/data/inst_plots/make_inst_plot_split.py
@@ -774,5 +774,5 @@ for ticklabel in ax37.yaxis.get_ticklabels():
 
 
 # save to pdf with right bounding box
-plt.savefig("../../figures/instpar_CG_gain_v1.pdf", bbox_inches='tight', bbox_extra_artists=[],pad_inches=0.03)
+#plt.savefig("../../figures/instpar_CG_gain_v1.pdf", bbox_inches='tight', bbox_extra_artists=[],pad_inches=0.03)
 
