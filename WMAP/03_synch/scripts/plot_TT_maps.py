649c04a6406e45325634b86593e7cb82c843a8fd
Duncan <duncanjosephwatts@gmail.com>
2023-09-15 17:13:51 +0200

    Updated some figures

--- WMAP/03_synch/scripts/plot_TT_maps.py
+++ WMAP/03_synch/scripts/plot_TT_maps.py
@@ -34,7 +34,7 @@ cg.plot(sd_beta_persamp, min=0, max=0.15, cmap='viridis')
 fnames = glob('TT*.fits')
 fnames.sort()
 
-labels = [r'\mathrm{Cosmoglobe}', 'CG K/CG Ka', r'\mathit{WMAP9}/\mathrm{PR3}', 
+labels = [r'\mathrm{Cosmoglobe}', r'\mathit{K/Ka}', r'\mathit{WMAP9}/\mathrm{PR3}', 
         r'\mathit{WMAP9}/\mathrm{PR4}', r'\mathit{WMAP9}/\mathrm{BP}']
 fignames = ['CG_K30', 'CG_KKa', 'W9PR3_K30', 'W9PR4_K30', 'W9BP_K30']
 for i, f in enumerate(fnames):
@@ -42,11 +42,19 @@ for i, f in enumerate(fnames):
         llabel = r'\beta_{\mathrm{s}}'
     else:
         llabel = ''
-    cg.plot(f,
-            min=-5, max=-1, llabel=llabel, width=columnwidth,
-            cbar=False,  rlabel=labels[i], 
-            fontsize={'rlabel':8, 'llabel':10}
-            )
+    if i == 1:
+        cg.plot(f,
+                min=-3.5, max=-2.5, width=columnwidth,
+                rlabel=labels[i], llabel=r'\beta_{\mathrm{s}}',
+                fontsize={'rlabel':8, 'llabel':10},
+                cbar=False,
+                )
+    else:
+        cg.plot(f,
+                min=-5, max=-1, llabel=llabel, width=columnwidth,
+                cbar=False,  rlabel=labels[i], 
+                fontsize={'rlabel':8, 'llabel':10}
+                )
     plt.savefig(f'../figures/TT_map_{fignames[i]}.pdf', bbox_inches='tight', dpi=150)
     plt.close()
 
@@ -56,3 +64,15 @@ cg.standalone_colorbar("planck",
         extend='both',
         width=0.8*columnwidth)
 plt.savefig('../figures/cbar_beta_wide.pdf', bbox_inches='tight', dpi=150)
+
+ticks = np.arange(-3.5, -2.4, 0.1)
+ticklabels = [''] * len(ticks)
+ticklabels[0] = r'$-3.5$'
+ticklabels[-1] = r'$-2.5$'
+ticklabels[5] = r'$-3.0$'
+cg.standalone_colorbar("planck", 
+        ticks=ticks,
+        ticklabels=ticklabels,
+        extend='both',
+        width=0.8*columnwidth)
+plt.savefig('../figures/cbar_beta.pdf', bbox_inches='tight', dpi=150)
