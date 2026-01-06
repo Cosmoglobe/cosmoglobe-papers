547d38bd65589f0883ce47dcfed7c2d11f2bfabd
Mathew <mathew.galloway@astro.uio.no>
2026-01-06 16:26:32 -0500

    edits and new plots for stars paper

--- DIRBE/04_starmodel/figs/starseds/star_seds.py
+++ DIRBE/04_starmodel/figs/starseds/star_seds.py
@@ -4,46 +4,87 @@ import h5py
 
 import matplotlib.pyplot as plt
 
-mean_emission = [0.5228979990674149, 0.483617344561016, 0.21522293944861112, 0.12503070382008852, 0.02770944012167716, 0.01172312982552367]
-
 dirbe_bands = [1.25, 2.2, 3.5, 4.9, 12, 25] #microns
-scalings = [1.82047631, 1.14461531, 0.56037995, 0.4172607, 0.04969875, 0.02000054]
+scalings = np.array([1.9566207, 1.31449094, 0.43703894, 0.24464909, 0.13159147, 0.07952643])
+stddevs = np.array([0.00333873, 0.00777074, 0.00488747, 0.00427976, 0.00988609, 0.01207999])
+
+chains_dir = '/mn/stornext/d23/cmbco/dirbe/DR2_v3.02/'
+
+gaia_cats = ['/mn/stornext/d23/cmbco/dirbe/data/mar25_mag12_5arcsec_withmcmc_mag7cutoff_parametrized_template_1.h5', '/mn/stornext/d23/cmbco/dirbe/data/mar25_mag12_5arcsec_withmcmc_mag7cutoff_parametrized_template_2.h5', '/mn/stornext/d23/cmbco/dirbe/data/mar25_mag12_5arcsec_withmcmc_mag7cutoff_parametrized_template_3.h5']
 
 
-chains_dir = '/mn/stornext/d23/cmbco/dirbe/DR2_v2.00/'
-catalog1 = '/mn/stornext/d23/cmbco/dirbe/data/commander_star_model_v3_feb25_mag8_5arcsec_7magcutoff_parametrized_template_1.h5'
-catalog2 = '/mn/stornext/d23/cmbco/dirbe/data/commander_star_model_v3_feb25_mag8_5arcsec_7magcutoff_parametrized_template_2.h5' 
+n_samps = 485
+n_burnin = 50
 
-n_samps = 200
-n_burnin = 10
+chains = ['chains_dirbe_prod6_c1', 'chains_dirbe_prod6_c2', 'chains_dirbe_prod6_c3', 'chains_dirbe_prod6_c4','chains_dirbe_prod6_c5']#,'chains_dirbe_prod6_c6']
 
-chains = ['chains_prod4_c1', 'chains_prod4_c2']#, 'chains_v1.02_c03', 'chains_v1.02_c04','chains_v1.02_c05','chains_v1.02_c06']
+count = 1
 
-count = 0
+data1 = []
+medians = [[],[],[],[],[],[]]
 
-data1 = np.zeros(135486)
-data2 = np.zeros(46623)
+cat1 = h5py.File(gaia_cats[0])
+cat2 = h5py.File(gaia_cats[1])
+cat3 = h5py.File(gaia_cats[2])
+
+
+catdata1 = cat1['reported_values'][()]
+catdata2 = cat2['reported_values'][()]
+catdata3 = cat3['reported_values'][()]
+
 
 for chain in chains:
 
     chainfile = h5py.File(chains_dir + chain + '/chain_c0001.h5')
 
-    for i in range(n_burnin, n_samps):
-        try:
-            data1 += chainfile['/' + str(i).zfill(6) + '/gaia1/amp'][:][0]
-            data2 += chainfile['/' + str(i).zfill(6) + '/gaia2/amp'][:][0]
-            count+= 1
-        except KeyError:
-            break
+    for i in range(n_burnin, n_samps, 2):
+        if(len(data1) == 0) :
+            data1 = chainfile['/' + str(i).zfill(6) + '/gaia1/amp'][:][0]
+            data2 = chainfile['/' + str(i).zfill(6) + '/gaia2/amp'][:][0]
+            data3 = chainfile['/' + str(i).zfill(6) + '/gaia3/amp'][:][0]
+            for j,band in enumerate(dirbe_bands):
+                new_data1 = data1[:]/catdata1[:,0]*catdata1[:,j]
+                new_data2 = data2[:]/catdata2[:,0]*catdata2[:,j]
+                new_data3 = data3[:]/catdata3[:,0]*catdata3[:,j]
+
+                new_data = np.append(new_data1, new_data2)
+                new_data = np.append(new_data, new_data3)
+
+                new_data = new_data[new_data > 0]
+
+                medians[j].append(np.median(new_data))
+
+        else:
+
+            try:
+                data1 += chainfile['/' + str(i).zfill(6) + '/gaia1/amp'][:][0]
+                data2 += chainfile['/' + str(i).zfill(6) + '/gaia2/amp'][:][0]
+                data3 += chainfile['/' + str(i).zfill(6) + '/gaia3/amp'][:][0]
+                
+                for j,band in enumerate(dirbe_bands):
+                    new_data1 = chainfile['/' + str(i).zfill(6) + '/gaia1/amp'][:][0]/catdata1[:,0]*catdata1[:,j]
+                    new_data2 = chainfile['/' + str(i).zfill(6) + '/gaia2/amp'][:][0]/catdata2[:,0]*catdata2[:,j]
+                    new_data3 = chainfile['/' + str(i).zfill(6) + '/gaia3/amp'][:][0]/catdata3[:,0]*catdata3[:,j]
+
+                    new_data = np.append(new_data1, new_data2)
+                    new_data = np.append(new_data, new_data3)
+                    new_data = new_data[new_data > 0]
+
+                    medians[j].append(np.median(new_data))
+
+                count+= 1
+            except KeyError:
+                break
 
 print(count)
 data1 = data1 / count
 data2 = data2 / count
+data3 = data3 / count
 
-cat1 = h5py.File(catalog1)
-cat2 = h5py.File(catalog2)
+cat1 = h5py.File(gaia_cats[0])
+cat2 = h5py.File(gaia_cats[1])
+cat3 = h5py.File(gaia_cats[2])
 
-plt.figure()
 
 #plt.plot(dirbe_bands, data[0]/cat['reported_values'][0][0]*cat['reported_values'][0][0:6], alpha=0.1, color='blue', label='Star SEDs')
 
@@ -52,36 +93,69 @@ plt.figure()
 #        print(i)
 #    plt.plot(dirbe_bands, data[i]/cat['reported_values'][i][0]*cat['reported_values'][i][0:6], alpha=0.1, color='blue')
 
+means = np.zeros(6)
+
+plt.figure()
 for i, band in enumerate(dirbe_bands):
-    #bin each set of SEDs vertically in 10000 bins
-   
-   
-    catdata1 = cat1['reported_values'][()]
-    catdata2 = cat2['reported_values'][()]
- 
+
     bin_data1 = data1[:]/catdata1[:,0]*catdata1[:,i]
     bin_data2 = data2[:]/catdata2[:,0]*catdata2[:,i]
+    bin_data3 = data3[:]/catdata3[:,0]*catdata3[:,i]
 
     bin_data = np.append(bin_data1, bin_data2)
+    bin_data = np.append(bin_data, bin_data3)
+
+    bins = np.logspace(1e-12, 10, num=50)
+    plt.hist(bin_data, label=str(band), log=True, bins=bins)
+
+plt.xscale('log')
+plt.legend(loc='best')
+plt.savefig('testhists.pdf')
+
+
+plt.figure()
+for i, band in enumerate(dirbe_bands):
+    #bin each set of SEDs vertically in 10000 bins
+    
+    bin_data1 = data1[:]/catdata1[:,0]*catdata1[:,i]
+    bin_data2 = data2[:]/catdata2[:,0]*catdata2[:,i]
+    bin_data3 = data3[:]/catdata3[:,0]*catdata3[:,i]
+
+    bin_data = np.append(bin_data1, bin_data2)
+    bin_data = np.append(bin_data, bin_data3)
 
     bin_data = bin_data[bin_data > 0]
 
     print(np.shape(bin_data), bin_data[0], bin_data[3000])
+    print(np.mean(bin_data), np.max(bin_data), np.min(bin_data))
 
-    plot = plt.violinplot(bin_data, positions=[band], side='high', widths=1, showextrema=False)
+    #plot = plt.violinplot(bin_data, positions=[band], side='high', widths=1, showextrema=False)
 
-    for bd in plot['bodies']:
-        bd.set_color('blue')
+    means[i] = np.median(bin_data)
+
+    #for bd in plot['bodies']:
+    #    bd.set_color('blue')
 
 
 #plt.plot(dirbe_bands, scalings, color='red', label='Full Sky Mean')
 
+#plt.plot(dirbe_bands, means, color='orange', label='Median SED')
+#plt.plot(dirbe_bands, scalings * (means[0] / scalings[0]), color='red', label='Diffuse SED')
+
+print(np.std(medians, axis=1))
+
+plt.errorbar(dirbe_bands, means, yerr=np.std(medians, axis=1), color='orange', label='Stars Median')
+
+plt.errorbar(dirbe_bands, scalings * (means[0] / scalings[0]), yerr=stddevs*(means[0] / scalings[0]), color='red', label='Diffuse SED')
 
 plt.yscale('log')
+#plt.ylim(1e-12, 10)
+
+#plt.xscale('log')
+
 plt.xlabel('Wavelength ($\mu m$)')
-plt.ylabel('Amplitude (mJy/Sr)')
-#plt.legend(loc='best')
+plt.ylabel('Amplitude (MJy/sr)')
+plt.legend(loc='best')
 
-
-plt.title('Star SEDs')
+#plt.title('Star SEDs')
 plt.savefig('star_seds.pdf')
